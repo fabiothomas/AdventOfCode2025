551b652f2e968fecb5d300f338ae4ba86624817a
Fabio van 't Hoff <gwnfabiothomas@gmail.com>
2025-12-01 11:38:08 +0100

    remove generated files

--- advent7/advent.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var content = fs.readFileSync("advent.txt", "utf-8");
-content.split("\n").forEach(function (line) {
-});
-console.log("complete");
