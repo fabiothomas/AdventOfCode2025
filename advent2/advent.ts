41faf70a777123cf1b42841478729a6ed9ab5a8d
Fabio van 't Hoff <gwnfabiothomas@gmail.com>
2025-12-02 10:41:16 +0100

    uncleaned but working day 2

--- advent2/advent.ts
+++ advent2/advent.ts
@@ -2,8 +2,23 @@ import * as fs from "fs";
 
 const content = fs.readFileSync("advent.txt", "utf-8");
 
-content.split("\n").forEach(line => {
-    
+let invalidSum = 0;
+
+// check if id is made of repeated parts
+const checkInValid = (id: string, size: number, part: string): boolean => id.length < size ? true : id.slice(0, size) !== part ? false : checkInValid(id.slice(size), size, part);
+
+content.split(",").forEach(line => {
+    const [start, end] = line.split("-").map(x => parseInt(x));
+    for (let i = start; i <= end; i++) {
+        let text = i.toString();
+        let result = false;
+        for (let j = 1; j < text.length; j++) {
+            if (text.length % j !== 0) continue;
+            result = checkInValid(text, j, text.slice(0, j));
+            if (result) break;
+        }
+        invalidSum += result ? i : 0;
+    }
 });
 
-console.log("complete");
+console.log(`Sum of invalid ids: ${invalidSum}`);
