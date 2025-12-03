2f0d2658c9759e0e148afc52814971c8ba9d57ea
Fabio van 't Hoff <gwnfabiothomas@gmail.com>
2025-12-03 13:18:56 +0100

    day 1

--- advent1/advent.ts
+++ advent1/advent.ts
@@ -5,6 +5,7 @@ const content = fs.readFileSync("advent.txt", "utf-8");
 let state: number = 50;
 let zerosLanded: number = 0;
 let zerosPassed: number = 0;
+const zerosFound = () => zerosLanded + zerosPassed;
 
 // result of modulo operation with iteration count
 interface ModResult {
@@ -12,20 +13,24 @@ interface ModResult {
     iterations: number;
 }
 
-// get total zeros found
-const zerosFound = () => zerosLanded + zerosPassed;
-
 // apply rotation to given state
-const rotate = (dir: string, state: number, add: number) => add * (dir === 'L' ? -1 : 1) + state;
+const rotate = (dir: string, state: number, add: number) => 
+    add * (dir === 'L' ? -1 : 1) + state;
 
 // read rotation from specific notation and execute
-const execute = (step: string, state: number) => rotate(step[0], state, parseInt(step.slice(1)));
+const execute = (step: string, state: number) => 
+    rotate(step[0], state, parseInt(step.slice(1)));
 
 // modulo with tracking of iterations
-const modWithTracking = (n: number, i = 0): ModResult => (0 <= n && n < 100) ? { value: n, iterations: i } : modWithTracking(n < 0 ? n + 100 : n - 100, i + 1);
+const modWithTracking = (n: number, i = 0): ModResult => 
+    0 <= n && n < 100 ? { value: n, iterations: i } : 
+    modWithTracking(n < 0 ? n + 100 : n - 100, i + 1);
 
 // get correction for zeros passed based on specific edge cases
-const getCorrection = (oldState: number, newState: number) => oldState === 0 && newState < 0 ? -1 : newState > 0 && newState % 100 === 0 ? -1 : 0;
+const getCorrection = (oldState: number, newState: number) => 
+    oldState === 0 && newState < 0 ? -1 :
+    newState > 0 && newState % 100 === 0 ? -1 :
+    0;
 
 content.split("\n").forEach(line => {
     let oldState = state;
